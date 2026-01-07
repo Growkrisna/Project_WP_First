9e8e3ef5c62bfde36ee66a6133b883d7dbc5a252
Growkrisna <krisnawcw@gmail.com>
2026-01-08 01:31:08 +0700

    ganti effect paralax

--- script.js
+++ script.js
@@ -3,11 +3,3 @@ const headerBg = document.querySelector('.header-bg');
 function isDesktop() {
   return window.innerWidth > 768;
 }
-
-window.addEventListener('scroll', () => {
-  if (!isDesktop()) return;
-
-  const scrollY = window.scrollY;
-  headerBg.style.transform = `translateY(${scrollY * 0.3}px)`;
-});
-
